134f7558cd230742004bf6f23c1b3b9c53c591e9
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2014-04-04 09:52:06 -0700

    Slightly more useful example.

--- post-clone-example.sh
+++ post-clone-example.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo "Hello, world!"
+echo "Just an example...I'm not changing anything.  Edit $0 to do the right thing."
+echo "Maybe set user.name and user.email..."
+# git config user.name "Jane Doe"
+# git config user.email "jdoe@example.com"
